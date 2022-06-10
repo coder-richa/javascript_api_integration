9916a9ab4573c20b506b13ca70d7db81df7ccd04
Richa <coder.richa@gmail.com>
2022-06-10 21:07:48 +0530

    Third party API integration for corelation coefficient calculation

--- js/script.js
+++ js/script.js
@@ -68,6 +68,7 @@ const fetchRates = async (url, parameterName = "AVG.INTWO") => {
       values.push(Number(observation[parameterName]?.v ?? 0));
     });
   }
+  
   // Return the response
   return { success, message, values };
 };
@@ -124,34 +125,6 @@ const calculateStats = (data = []) => {
   };
 };
 
-// Function to calculate the pearson coefficient
-const pearson = (x = [], y = []) => {
-  const promedio = (l) => l.reduce((s, a) => s + a, 0) / l.length;
-  const calc = (v, prom) =>
-    Math.sqrt(v.reduce((s, a) => s + a * a, 0) - n * prom * prom);
-  let n = x.length;
-  let nn = 0;
-  for (let i = 0; i < n; i++, nn++) {
-    if ((!x[i] && x[i] !== 0) || (!y[i] && y[i] !== 0)) {
-      nn--;
-      continue;
-    }
-    x[nn] = x[i];
-    y[nn] = y[i];
-  }
-  if (n !== nn) {
-    x = x.splice(0, nn);
-    y = y.splice(0, nn);
-    n = nn;
-  }
-  const prom_x = promedio(x),
-    prom_y = promedio(y);
-  return (
-    (x.map((e, i) => ({ x: e, y: y[i] })).reduce((v, a) => v + a.x * a.y, 0) -
-      n * prom_x * prom_y) /
-    (calc(x, prom_x) * calc(y, prom_y))
-  );
-};
 
 // Present data stats in table
 const displayStats = (data, sectionDiv) => {
@@ -214,7 +187,7 @@ const searchHandler = async (e) => {
     displayStats(usdCADStats, usdCADRateDiv);
 
     // Update pearson correlation coefficient
-    const pearsonValue = formatFloat(pearson(corraData, usdCADData));
+    const pearsonValue = formatFloat(this.spearson.correlation.pearson(corraData, usdCADData));
     const pearsonLbl = $first("#pearson-val");
     updateContent(pearsonLbl, pearsonValue);
 
